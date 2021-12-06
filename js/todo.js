b8ee88caa80ad0e428b63a43e27f8c8df1c66412
K-J-HYEON <kjh950601@naver.com>
2021-12-06 17:52:13 +0900

    JS-Chrome

--- js/todo.js
+++ js/todo.js
@@ -2,14 +2,29 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintToDo(newTodo) {
+    const li = document.createElement("li")
+    const span = document.createElement("span")
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "💢";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
 
 
-function handleToDoSubmit(event) {
+function handleToDoSubmit(event) { 
     event.preventDefault();
     const newTodo = toDoInput.value;
-    console.log(toDoInput.value);
     toDoInput.value = "";
-    console.log(newTodo, toDoInput.value);
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
